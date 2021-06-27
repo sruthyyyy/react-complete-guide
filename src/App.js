06f75b6cc9c4e79c704294debec8a3fff95d9c05
sruthyyyy <careerssruthy96@gmail.com>
2021-06-27 22:40:04 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-
-import ExpenseItem from "./components/ExpenseItem"
-
+import ExpenseItem from './components/ExpenseItem'
+import react from React
 function App() {
   return (
     <div>
       <h1>Let;s get started..</h1>
-     <ExpenseItem/>
     </div>
   );
 }
-export default App
+export default App;
